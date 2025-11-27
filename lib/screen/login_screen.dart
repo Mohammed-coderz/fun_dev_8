ba30d31feac54961702ce3fa32177e73dcff57d4
Mohammed-coderz <m.AbuElhawa@coderz-tech.com>
2025-11-27 17:32:05 +0300

    feat: Implement "remember me" functionality
    This commit introduces a "remember me" feature on the login screen using `shared_preferences`.
    
    - Adds `shared_preferences` as a dependency.
    - Creates `SharedPreferencesHelper` to abstract away shared preferences logic.
    - Implements a "remember me" checkbox on the login screen.
    - Updates the login button logic to save the checkbox state.
    - Includes iOS configuration updates related to adding a new package.
    - Sets the `LoginScreen` as the initial route for the app.

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fun_dev_8/screen/home_screen.dart';
 import 'package:fun_dev_8/screen/signup.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../core/utils/shared_preferences_helper.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,6 +13,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +28,7 @@ class _LoginScreenState extends State<LoginScreen> {
           color: Colors.white,
           child: SizedBox(
             width: 350,
-            height: 450,
+            height: 550,
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 20),
               child: Column(
@@ -59,12 +64,47 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                   SizedBox(height: 25),
+                  Row(
+                    children: [
+                      Checkbox(
+                        checkColor: Colors.white,
+                        value: isChecked,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            isChecked = value!;
+                          });
+                        },
+                      ),
+                      SizedBox(width: 10),
+                      Text("remember me"),
+                    ],
+                  ),
+                  SizedBox(height: 25),
                   ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => HomeScreen()),
+                    onPressed: () async {
+                      // final SharedPreferences prefs =
+                      //     await SharedPreferences.getInstance();
+
+                      print("ischecked : $isChecked");
+
+                      // final bool? mohammed = prefs.getBool('isRememberMe');
+                      final bool? isRememberMe =
+                          await SharedPreferencesHelper.getBool('isRememberMe');
+
+                      print("isRememberMe : $isRememberMe");
+                    },
+                    child: Text("check SharedPreferences"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      await SharedPreferencesHelper.saveBool(
+                        'isRememberMe',
+                        isChecked,
                       );
+                      // Navigator.pushReplacement(
+                      //   context,
+                      //   MaterialPageRoute(builder: (context) => HomeScreen()),
+                      // );
                     },
                     child: Text("Login"),
                   ),
